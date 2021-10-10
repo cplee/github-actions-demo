07d7ff85e8b57bbe472444f7ce44a170d827ed26
Etty <beltrami.ester@gmail.com>
2021-10-10 14:04:20 +0100

    Try

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/plain');
   res.send('Hello World');
+  res.send("wrf")
 });
 
 module.exports = app.listen(8080, () => {
